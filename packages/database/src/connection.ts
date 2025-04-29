691594cc91b32d8faa67f8f5fd1270d562debd51
jitendra-techuz <jitendra@techuz.com>
2025-04-29 14:43:40 +0530

    Configure the db query logs

--- packages/database/src/connection.ts
+++ packages/database/src/connection.ts
@@ -1,13 +1,13 @@
 import { Model as ObjectionModel } from 'objection';
 import knexGenerator from 'knex';
 import { appConfig } from '@repo/config';
-import { log } from '@repo/logger';
 import db from './knexfile';
+import { logDbQueries } from './logDBQueries';
 
 const knex = knexGenerator({ ...db.development });
 const Model = ObjectionModel.knex(knex);
 
 // LOG_DB_QUERIES
-if (appConfig.logDbQueries) knex.on('query', (query) => log.info(query['sql']));
+if (appConfig.logDbQueries) logDbQueries(knex);
 
 export { Model, knex };
