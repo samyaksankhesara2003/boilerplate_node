569bd5285ce0de27632fe74fcd7edc338e4217bc
jitendra-techuz <jitendra@techuz.com>
2025-04-30 16:41:33 +0530

    create a utility function for random number and string

---
+++ packages/utils/src/__tests__/random.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "@jest/globals";
+import { generateRandomNumber, generateRandomString } from '../random/random';
+
+describe('generateRandomNumber', () => {
+    it('should return a number within range', () => {
+        const min = 5, max = 15;
+        const num = generateRandomNumber(min, max);
+        expect(num).toBeGreaterThanOrEqual(min);
+        expect(num).toBeLessThanOrEqual(max);
+    });
+});
+
+describe('generateRandomString', () => {
+    it('should generate string of correct length', () => {
+        const str = generateRandomString(12);
+        expect(str.length).toBe(12);
+        expect(/^[a-zA-Z0-9]+$/.test(str)).toBe(true);
+    });
+});
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
 export * from './crypto/password';
 export * from './format/date';
 export * from './validation/email';
+export * from './random/random';
