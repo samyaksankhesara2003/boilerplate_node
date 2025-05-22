298b9fa4ef879893523c923765a187be6fdc1056
Jainam Shah <jainam@techuz.com>
2025-05-22 17:55:54 +0530

    redis in adminMiddleware

--- apps/api/src/middlewares/adminAuth.middleware.ts
+++ apps/api/src/middlewares/adminAuth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { User } from '@repo/db';
 import { log } from '@repo/logger';
 import { jwtUtil } from '@repo/tokens';
-import { constants } from '@repo/config';
+import { constants, jwtConfig } from '@repo/config';
 import { StatusCodes, ResponseMessages, sendResponse } from '@repo/response-handler';
 import { getRedisData, setRedisData } from '@repo/redis';
 
@@ -15,20 +15,16 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
     if (!token) return sendResponse(res, StatusCodes.UNAUTHORIZED, ResponseMessages.COMMON.NOT_AUTHENTICATED);
 
     try {
-        const data = await getRedisData(token);
-        if (data) {
-            req.user = data;
-            console.log('redis')
-            return next();
-        }
-
-        console.log('no redis')
-
         const decoded = jwtUtil.validateJwt(token);
         if (!decoded) return sendResponse(res, StatusCodes.UNAUTHORIZED, ResponseMessages.COMMON.NOT_AUTHENTICATED);
 
-        const attributes = ['id', 'first_name', 'last_name', 'email', 'profile_url', 'token', 'role', 'status', 'slug'];
-        let user = await User.query().select(...attributes).findById(decoded.data.id);
+        let user: User | undefined;
+        const data = await getRedisData(token);
+        if (data) user = data;
+        else {
+            const attributes = ['id', 'first_name', 'last_name', 'email', 'profile_url', 'token', 'role', 'status', 'slug'];
+            user = await User.query().select(...attributes).findById(decoded.data.id);
+        }
 
         if (!user) return sendResponse(res, StatusCodes.UNAUTHORIZED, ResponseMessages.COMMON.NOT_AUTHENTICATED);
         if (user.token !== token) return sendResponse(res, StatusCodes.UNAUTHORIZED, ResponseMessages.COMMON.NOT_AUTHENTICATED);
@@ -36,7 +32,7 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
             return sendResponse(res, StatusCodes.UNAUTHORIZED, ResponseMessages.COMMON.NOT_AUTHENTICATED);
         }
 
-        await setRedisData(token, user);
+        await setRedisData(token, user, jwtConfig.jwtExpiresIn);
         req.user = user;
         return next();
     } catch (error) {
--- packages/database/src/seeds/3_admins.ts
+++ packages/database/src/seeds/3_admins.ts
@@ -20,6 +20,7 @@ export async function seed(knex: Knex): Promise<void> {
             last_name: "Admin",
             email: "admin@yopmail.com",
             profile_url: null,
+            slug: 'techuz-admin',
             // password: "$2b$10$92IXUNpkjO0rOQ5by",
             token: null,
             reset_password_token: null,
