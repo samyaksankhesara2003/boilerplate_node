fa6f365f5eca9d90a692a108bc1bc24653129fad
jitendra-techuz <jitendra@techuz.com>
2025-06-02 15:09:12 +0530

    WIP: Update the logger package and create a http logger middleware

--- packages/config/src/__tests__/logger.config.test.ts
+++ packages/config/src/__tests__/logger.config.test.ts
@@ -7,5 +7,6 @@ describe('loggerConfig', () => {
         expect(loggerConfig.enableFile).toEqual(process.env.ENABLE_FILE_LOG === 'true');
         expect(loggerConfig.logFilePath).toEqual(process.env.LOG_FILE_PATH || '../../logs/app.log');
         expect(loggerConfig.pretty).toEqual(process.env.NODE_ENV !== 'production');
+        expect(loggerConfig.maxsize).toEqual(+process.env.LOG_FILE_MAX_SIZE! || 5 * 1024 * 1024);
     });
 });
--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,40 +1,81 @@
-import pino from 'pino';
 import fs from 'fs';
 import path from 'path';
+import { createLogger, format, transports } from 'winston';
 import { loggerConfig } from '@repo/config';
 
-const { logLevel, enableFile, logFilePath } = loggerConfig;
+const { logLevel, enableFile, logFilePath, maxsize } = loggerConfig;
 
-const transportStreams: Array<pino.TransportTargetOptions | pino.TransportPipelineOptions> = [
-    {
-        target: 'pino-pretty',
-        level: logLevel,
-        options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname'
-        }
-    }
-];
-
-if (enableFile) {
-    const dir = path.dirname(logFilePath);
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-
-    transportStreams.push({
-        target: 'pino/file',
-        level: logLevel,
-        options: { destination: logFilePath, mkdir: true }
-    });
+const logDir = path.dirname(logFilePath);
+if (enableFile && !fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir, { recursive: true });
 }
 
-export const log = pino(
-    {
-        level: logLevel
-    },
-    pino.transport({
-        targets: transportStreams
-    })
-);
+/**
+ * @author Jitendra Singh
+ * @description Checks if the given metadata object is an array-like object.
+ * @param meta - The metadata object to be checked.
+ * @returns The length of the array-like object if true, false otherwise.
+ */
+const isArrayLikeMeta = (meta: Record<string, any>): number | boolean => {
+    const keys = Object.keys(meta);
+    return keys.length && keys.every(key => /^\d+$/.test(key));
+};
+
+/**
+ * @author Jitendra Singh
+ * @description Builds a formatted JSON string from the provided metadata object.
+ * @param meta - The metadata object to be converted to a JSON string.
+ * @returns A formatted JSON string representation of the metadata, or an empty string if no metadata is provided.
+ */
+const buildMetaString = (meta: Record<string, any>): string => {
+    if (!meta || Object.keys(meta).length === 0) return '';
+
+    if (isArrayLikeMeta(meta)) {
+        const arr = Object.values(meta);
+        return `\n${JSON.stringify(arr, null, 2)}`;
+    }
+
+    return `\n${JSON.stringify(meta, null, 2)}`;
+};
+
+const log = createLogger({
+    level: logLevel,
+    format: format.combine(format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), format.errors({ stack: true }), format.splat(), format.json()),
+    transports: [
+        new transports.Console({
+            level: logLevel,
+            format: format.combine(
+                format.colorize(),
+                format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                format.printf(({ timestamp, level, message, ...meta }) => {
+                    const msg = typeof message === 'object' ? JSON.stringify(message, null, 2) : message;
+
+                    const metaString = buildMetaString(meta);
+                    return `[${timestamp}] ${level}: ${msg}${metaString}`;
+                })
+            )
+        }),
+        ...(enableFile
+            ? [
+                new transports.File({
+                    filename: logFilePath,
+                    level: logLevel,
+                    maxsize: maxsize || 5 * 1024 * 1024, // 5MB
+                    maxFiles: 5,
+                    tailable: true,
+                    // format: format.combine(
+                    //     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+                    //     format.printf(({ timestamp, level, message, ...meta }) => {
+                    //         const msg = typeof message === 'object' ? JSON.stringify(message, null, 2) : message;
+                    //         const metaString = buildMetaString(meta);
+                    //         return `[${timestamp}] ${level}: ${msg}${metaString}`;
+                    //     })
+                    // )
+                })
+            ]
+            : [])
+    ],
+    exitOnError: false
+});
+
+export { log };
